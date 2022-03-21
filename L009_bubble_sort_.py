b66b4b360f06017caeab8938b093ff9115759a47
MarcinCichy <marcin-cichy@wp.pl>
2022-03-21 23:17:25 +0000

    Replace L009_bubble_sort_.py

--- L009_bubble_sort_.py
+++ L009_bubble_sort_.py
@@ -13,28 +13,22 @@ def bubble_sort(datas):
 			temp = datas[j-1]
 			datas[j-1] = datas[j]
 			datas[j] = temp
-			print(i,'.', j, ' = ', datas)
+			print(i, '.', j, ' = ', datas)
 	print("*"*40)
 	print(f"Lista posortowana: {datas}")
 
 
 print("*"*40)
-
-
-l = [[5, 4, 3, 2, 1], [1, 1, 1, 1, 1], [], [1, 5, 7, 10, 11], [23, 5, 7, 46, 78, 98, 1, 4, 7989, 3, 1]]
-
-for i in range (5):
-	lista = l[i]
+listy = [[5, 4, 3, 2, 1], [1, 1, 1, 1, 1], [], [1, 5, 7, 10, 11], [23, 5, 7, 46, 78, 98, 1, 4, 7989, 3, 1]]
+for i in range(5):
+	lista = listy[i]
 	print(f"Lista wejściowa:  {lista} \n")
 	bubble_sort(lista)
 	print("\n")
 	print("*" * 40)
-	
 
-
-
-l1 = [5, 4, 3, 2, 1]
-l2 = [1, 1, 1, 1, 1]
-l3 = []
-l4 = [1, 5, 7, 10, 11]
-l5 = [23, 5, 7, 46, 78, 98, 1, 4, 7989, 3, 1]
+# l1 = [5, 4, 3, 2, 1]
+# l2 = [1, 1, 1, 1, 1]
+# l3 = []
+# l4 = [1, 5, 7, 10, 11]
+# l5 = [23, 5, 7, 46, 78, 98, 1, 4, 7989, 3, 1]
